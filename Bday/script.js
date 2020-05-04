bd41d1f968b67209450d99009b693fdba3ad0c74
pruthvirajc <pruthviwhatnext@gmail.com>
2020-05-04 21:34:14 +0530

    birthday wish

--- Bday/script.js
+++ Bday/script.js
@@ -1,12 +1,15 @@
 const ele2017 = document.getElementById('content-2017');
 const ele2018 = document.getElementById('content-2018');
 const ele2019 = document.getElementById('content-2019');
+const slideButton = document.getElementById('slide');
 
 document.getElementById('video').controls = true;
 document.getElementById('video-1').controls = true;
 document.getElementById('video-2').controls = true;
 document.getElementById('video-3').controls = true;
 document.getElementById('video-4').controls = true;
+document.getElementById('cutid1').controls = true;
+document.getElementById('cutid2').controls = true;
 
 function loopGokarna() {
   for (let i = 1; i <= 8; i++) {
@@ -15,15 +18,12 @@ function loopGokarna() {
     ele.src = `resources/2017/gokarna${i}.jpg`;
     ele2017.appendChild(ele);
 
-
-    
-    if(i===7){
-    const ele1 = document.createElement('div');
-    ele1.classList.add('div-container');
-    ele1.innerHTML = "Fashionable Lady 🤪";
-    ele2017.appendChild(ele1);
+    if (i === 7) {
+      const ele1 = document.createElement('div');
+      ele1.classList.add('div-container');
+      ele1.innerHTML = 'Fashionable Lady 🤪';
+      ele2017.appendChild(ele1);
     }
-
   }
 }
 
@@ -35,7 +35,6 @@ function loopChick() {
     ele.controls = true;
     ele2018.appendChild(ele);
     console.log(ele);
-  
   }
 
   for (let i = 1; i <= 1; i++) {
@@ -73,70 +72,75 @@ loopGokarna();
 loopChick();
 loopMysore();
 
-
-
-var palette = ['#eb4d4b', '#badc58', '#f9ca24', '#22a6b3']
+var palette = ['#eb4d4b', '#badc58', '#f9ca24', '#22a6b3'];
 
 function random(min = 0, max = 10) {
-  return Math.floor(Math.random() * (max - min + 1)) + min
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 function generateColor() {
-  let index = random(0, palette.length - 1)
-  return palette[index]
+  let index = random(0, palette.length - 1);
+  return palette[index];
 }
 
 function cleanUp(el) {
   while (el.firstChild) {
-    el.removeChild(el.firstChild)
+    el.removeChild(el.firstChild);
   }
 }
 
 function partyTime() {
   // remove exisiting elements before creaitng new ones
- const container = document.querySelector('.living-room')
-  cleanUp(container)
+  const container = document.querySelector('.living-room');
+  cleanUp(container);
 
-  let numberOfBalloons = (window.innerWidth / 80) * 3
+  let numberOfBalloons = (window.innerWidth / 80) * 3;
 
   for (var i = 0; i < numberOfBalloons; i++) {
-   
-    const template = document.querySelector('template').content.cloneNode(true)
+    const template = document.querySelector('template').content.cloneNode(true);
 
-    let thisColor = generateColor()
+    let thisColor = generateColor();
     let transform = {
       rotate: Math.floor(random(-15, 15) * 1.25) + 'deg',
       translateX: random(-window.innerWidth / 2, window.innerWidth / 2) + 'px',
-      translateY: random(-100, 100) + 'px',
-    }
+      translateY: random(-100, 100) + 'px'
+    };
 
     template.querySelector('.helium').style.transform = Object.keys(transform)
-      .map(prop => `${prop}(${transform[prop]})`).join(' ')
-    template.querySelector('.balloon').style.background = thisColor
-    template.querySelector('.knot').style.borderBottomColor = thisColor
+      .map(prop => `${prop}(${transform[prop]})`)
+      .join(' ');
+    template.querySelector('.balloon').style.background = thisColor;
+    template.querySelector('.knot').style.borderBottomColor = thisColor;
 
-    container.appendChild(document.importNode(template, true))
+    container.appendChild(document.importNode(template, true));
   }
 
-  document.querySelectorAll('.helium').forEach(e => e.addEventListener('click', function(ev) {
-    Object.assign(ev.target.style, {
-      width: `.${random(50, 75)}rem`,
-      height: '3px',
-      transform: `translateY(${window.innerHeight
-          * 0.9}px) skew(20deg,0deg) rotate(${random(0, 100)}deg)`,
-      transition: 'transform 1s ease-out',
-    })
-    ev.target.classList.add('popped')
+  document.querySelectorAll('.helium').forEach(e =>
+    e.addEventListener('click', function(ev) {
+      Object.assign(ev.target.style, {
+        width: `.${random(50, 75)}rem`,
+        height: '3px',
+        transform: `translateY(${window.innerHeight *
+          0.9}px) skew(20deg,0deg) rotate(${random(0, 100)}deg)`,
+        transition: 'transform 1s ease-out'
+      });
+      ev.target.classList.add('popped');
 
-    ev.target.querySelector('.string').style.opacity = 0
-    ev.target.querySelector('.knot').style.borderBottomWidth = '3px'
-    ev.target.querySelector('.knot').style.animationPlayState = 'paused'
-  }))
+      ev.target.querySelector('.string').style.opacity = 0;
+      ev.target.querySelector('.knot').style.borderBottomWidth = '3px';
+      ev.target.querySelector('.knot').style.animationPlayState = 'paused';
+    })
+  );
 }
 
 setInterval(partyTime, 7000);
 
+document.querySelector('.refresh').addEventListener('click', partyTime);
 
-document.querySelector('.refresh').addEventListener('click', partyTime)
+partyTime(document.querySelector('.living-room'));
 
-partyTime(document.querySelector('.living-room'))
+slideButton.addEventListener('click', () => {
+  console.log("into slide");
+  elelment = document.getElementById('slide-container');
+  elelment.classList.toggle('show');
+});
